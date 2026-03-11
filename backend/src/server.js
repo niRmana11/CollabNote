74e10e8fdecea780d599c57e98a3430d81ee8d5f
niRmana11 <nirmanaherath123@gmail.com>
2026-03-11 23:28:06 +0530

    implement MongoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
+
+// connect database
+connectDB();
 
 app.use(cors());
 app.use(express.json());
