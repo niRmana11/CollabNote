0b7b385801abf885b111add803bf4409503d3769
niRmana11 <nirmanaherath123@gmail.com>
2026-03-12 22:25:12 +0530

    initialize React frontend and configure Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-5xl font-bold text-white">Tailwind Working 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
